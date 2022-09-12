d28bdcfa35db60d0c0cfb1a60f0e577c4381f122
jakecoffman <jake@jakecoffman.com>
2022-09-12 09:56:13 -0500

    use system git since that's how Dependabot will need to run it

--- script.sh
+++ script.sh
@@ -8,7 +8,10 @@ cp /root/.mitmproxy/mitmproxy-ca-cert.cer /usr/local/share/ca-certificates/mitm.
 update-ca-certificates
 # Poetry seems to prefer this environment variable
 export REQUESTS_CA_BUNDLE="/root/.mitmproxy/mitmproxy-ca-cert.cer"
+# https://python-poetry.org/docs/configuration/#experimentalsystem-git-client
+# This doesn't seem to fix the issue.
+poetry config experimental.system-git-client true
 # Do the update
-poetry update fastapi
+poetry -vvv update fastapi
 # You can try what's on master with this line
 # ~/.local/bin/poetry@git update fastapi
